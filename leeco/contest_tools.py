6a7aeb6fa0cb83a7d7dc3c6aac3425337752cc95
jeza <jeza@vip.qq.com>
2024-08-07 19:11:44 +0800

    Add contest tools

---
+++ leeco/contest_tools.py
@@ -0,0 +1,21 @@
+""" Tools for LeetCode contests, only include necessary modules and functions. """
+
+from . import *
+from collections import deque, defaultdict
+import heapq
+import functools
+
+
+__all__ = [
+    # *_module_all,
+    # main functions
+    'inject', 'test',
+    # data structures
+    'ListNode', 'TreeNode',
+    # test case
+    'TestCase', 'Result',
+    # type hints
+    'List', 'Optional',
+    'deque', 'defaultdict',
+    'heapq', 'functools'
+]
